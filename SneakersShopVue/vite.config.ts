bbd42d3483e94eb13d21b97b59c63470d591f605
Alexander <128146473+Alexander-Guguchkin@users.noreply.github.com>
2024-09-22 19:36:27 +0300

    создание dockerfile  и  docker-compose для дальнейшего деплоя

---
+++ SneakersShopVue/vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  server:{
+    watch:{
+      usePolling: true
+    },
+    host: true,
+    strictPort:true,
+    port:8080
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
